9eebd67222909a28ffe0f722abb79a4c95aaf3ef
mahmoud karzoun <135722882+mkarson1997@users.noreply.github.com>
2026-09-12 15:56:57 +0300

    fix: satisfy Dart directive ordering

--- lib/src/app/default_incident_service.dart
+++ lib/src/app/default_incident_service.dart
@@ -1,7 +1,7 @@
-import 'package:incidentdeck/src/application/incident_service.dart';
 import 'package:incidentdeck/src/app/default_incident_service_stub.dart'
     if (dart.library.io) 'package:incidentdeck/src/app/default_incident_service_io.dart'
     as platform;
+import 'package:incidentdeck/src/application/incident_service.dart';
 
 Future<IncidentService> createDefaultIncidentService() {
   return platform.createDefaultIncidentService();
