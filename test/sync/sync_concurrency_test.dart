8e566ccd020a3baceeac1961d6a1974ffdf999d1
mahmoud karzoun <135722882+mkarson1997@users.noreply.github.com>
2026-09-12 18:29:11 +0300

    fix: remove redundant sync test import

--- test/sync/sync_concurrency_test.dart
+++ test/sync/sync_concurrency_test.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:incidentdeck/src/data/incident_repository.dart';
 import 'package:incidentdeck/src/domain/incident.dart';
